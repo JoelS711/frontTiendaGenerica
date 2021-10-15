288edeffb66e1b4fb81cc8a5d9df8de68fbf0b9e
Oscar Cita <smalqs@gmail.com>
2021-10-15 01:06:48 -0500

    Ventas Controller con funcion de crear

--- src/main/java/Ciclo3/front/controller/VentasController.java
+++ src/main/java/Ciclo3/front/controller/VentasController.java
@@ -1,5 +1,41 @@
 package Ciclo3.front.controller;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import Ciclo3.front.model.ClientesDAO;
+import Ciclo3.front.model.VentasDAO;
+import Ciclo3.front.vo.VentasVO;
+
+@Controller
 public class VentasController {
 
+	private ArrayList<VentasVO> listarVentas;
+
+	@Autowired
+	private VentasDAO vendao;
+	private ClientesDAO clidao;
+	
+	@PostMapping("/venta/crearVenta")
+	public String crearVenta(@Validated VentasVO venta, BindingResult resultadoValidacion, Model model) {
+		String redi;
+		vendao = new VentasDAO();
+		if (resultadoValidacion.hasErrors()) {
+			model.addAttribute("error", "Datos incorrectos");
+			redi = "/venta/Ventas";
+		} else {
+			model.addAttribute("usuario", vendao.crearVenta(venta));
+			redi = "/venta/Ventas";
+			model.addAttribute("mensaje", "Venta creada");
+		}
+		return redi;
+	}
+	
+	
 }
