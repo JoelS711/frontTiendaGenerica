eb9425073d6e1b132b95d7a442bc9eb24a75df95
Oscar Cita <smalqs@gmail.com>
2021-10-15 01:08:34 -0500

    Se complementa Ventas DAO y la clase VentasVO

--- src/main/java/Ciclo3/front/model/VentasDAO.java
+++ src/main/java/Ciclo3/front/model/VentasDAO.java
@@ -1,5 +1,37 @@
 package Ciclo3.front.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+
+import Ciclo3.front.vo.VentasVO;
+import reactor.core.publisher.Mono;
+
+@Service
 public class VentasDAO {
+private static final String URL = "http://localhost:8080";
+	
+@Autowired
+private WebClient webClient;
+
+public VentasVO crearVenta(VentasVO ven) {
+
+	try {
+		WebClient webClient = WebClient.create(URL);
+		VentasVO objVenta = null;
+		Mono<VentasVO> response = webClient.post().uri(URL + "/usuario/crearUsuario")
+				.body(Mono.just(ven), VentasVO.class).retrieve().bodyToMono(VentasVO.class);
+
+		objVenta = response.block();
+		return objVenta;
+
+	} catch (WebClientResponseException e) {
+		e.getMessage();
+		System.out.println("---->" + e.getMessage());
+		return null;
+	}
+
+}
 
 }
--- src/main/java/Ciclo3/front/vo/VentasVO.java
+++ src/main/java/Ciclo3/front/vo/VentasVO.java
@@ -1,5 +1,78 @@
 package Ciclo3.front.vo;
 
 public class VentasVO {
+	
+	private Long cedula_usuario;
+	private Long cedula_cliente;
+	private Long codigo;
+	private Long iva;
+	private Long total_venta;
+	private Long valor_venta;
+	
+	public VentasVO(Long cedula_usuario, Long cedula_cliente, Long codigo, Long iva, Long total_venta,
+			Long valor_venta) {
+		super();
+		this.cedula_usuario = cedula_usuario;
+		this.cedula_cliente = cedula_cliente;
+		this.codigo = codigo;
+		this.iva = iva;
+		this.total_venta = total_venta;
+		this.valor_venta = valor_venta;
+	}
+
+	@Override
+	public String toString() {
+		return "VentasVO [cedula_usuario=" + cedula_usuario + ", cedula_cliente=" + cedula_cliente + ", codigo="
+				+ codigo + ", iva=" + iva + ", total_venta=" + total_venta + ", valor_venta=" + valor_venta + "]";
+	}
+
+	public Long getCedula_usuario() {
+		return cedula_usuario;
+	}
+
+	public void setCedula_usuario(Long cedula_usuario) {
+		this.cedula_usuario = cedula_usuario;
+	}
+
+	public Long getCedula_cliente() {
+		return cedula_cliente;
+	}
+
+	public void setCedula_cliente(Long cedula_cliente) {
+		this.cedula_cliente = cedula_cliente;
+	}
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public Long getIva() {
+		return iva;
+	}
+
+	public void setIva(Long iva) {
+		this.iva = iva;
+	}
+
+	public Long getTotal_venta() {
+		return total_venta;
+	}
+
+	public void setTotal_venta(Long total_venta) {
+		this.total_venta = total_venta;
+	}
+
+	public Long getValor_venta() {
+		return valor_venta;
+	}
+
+	public void setValor_venta(Long valor_venta) {
+		this.valor_venta = valor_venta;
+	}
+	
 
 }
